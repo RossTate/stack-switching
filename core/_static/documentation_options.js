2258e01b97ae3920daad1ac2b98885efefa5285d
RossTate <RossTate@users.noreply.github.com>
2022-09-28 01:52:54 +0000

    deploy: c4d857483f458c7e72653133a64b693766d22539

---
+++ core/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '1.1 + function references + tail calls + stack-switching (Draft 2022-09-28)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
